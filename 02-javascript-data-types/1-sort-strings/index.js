0ff0195eb2b94414bab888d9dfff8021a35b6f7d
VUIVR <asostrovskiy@yandex.ru>
2023-11-19 13:59:19 +0400

    feat: add sort func

--- 02-javascript-data-types/1-sort-strings/index.js
+++ 02-javascript-data-types/1-sort-strings/index.js
@@ -4,6 +4,18 @@
  * @param {string} [param="asc"] param - the sorting type "asc" or "desc"
  * @returns {string[]}
  */
-export function sortStrings(arr, param = 'asc') {
 
+export function sortStrings(arr, param = 'asc') {
+	const cloneArr = [...arr]
+	if (param === 'desc') {
+		return cloneArr.sort((a, b) => {
+			return b.localeCompare(a, undefined, {caseFirst: 'upper'})
+		})
+	}
+	if (param === 'asc') {
+		cloneArr.sort((a, b) => {
+			return a.localeCompare(b, undefined, {caseFirst: 'upper'});
+		})
+	}
+	return cloneArr
 }
